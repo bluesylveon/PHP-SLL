878434e6d26bcd0d1baefab7ed5c0ea9a222a4b0
deltatager <simonllandry@hotmail.com>
2019-01-24 09:58:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "This should now print this! 3rrrrrddd #webception"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Page de test de Simon</title>
+		<meta charset="utf8">
+	</head>
+	<body>
+		<?php echo "Hello bitches."; ?>
+	</body>
+</html>
